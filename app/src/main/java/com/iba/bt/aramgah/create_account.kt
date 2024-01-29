e51392afe4d22e94c9f7875c5865c9e4b6538e77
Naveed Ahmed Shaikh <naveedshaikh2425770@gmail.com>
2024-01-29 15:22:08 +0500

    Naveed

---
+++ app/src/main/java/com/iba/bt/aramgah/create_account.kt
@@ -0,0 +1,11 @@
+package com.iba.bt.aramgah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class create_account : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
